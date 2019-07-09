589007b026ea294290ccd842a6512feb1b6d982f
Shirui Zhong <shirui.us@gmail.com>
2019-07-09 13:26:41 -0700

    Initial commit

---
+++ Contents.swift
@@ -0,0 +1,82 @@
+import UIKit
+
+var str = "Hello, playground"
+
+var num1 = 1
+var num2 = 2
+var num3 = 3
+//true or false
+//first one prints true
+//second one prints false
+//third one prints false
+//print(num1<num2)
+//print(num1>num2)
+//print(num1==num2)
+// checking if num1 is NOT equal to num2
+//print(num1 != num2)
+//prints true
+//checking if 1 + 2 = 3 AND 2 + 1 = 3, if its correct then does something (here it prints "yes!"
+var num4 = 4
+var num5 = 5
+var num6 = 6
+var num7 = 7
+
+if num5 == num7 {
+    print("yeah")
+}
+if num5 != num7 {
+    print("no")
+}
+
+var baby = 0.00000000001
+if baby > 1 {
+    print("It doesn't matter what I write here cause it shouldn't ever get printed if I did this right")
+}
+if baby < 1 {
+    print("Ogachakaogachakaka ogga oga")
+}
+
+ var dogAge = 1
+if dogAge < 2 {
+    print("You are a puppy")
+}
+if dogAge == 2 {
+    print("Woof woof")
+}
+if dogAge == 3 {
+    print("You are awesome!")
+}
+if dogAge == 4 {
+    print("You're okay I guess")
+}
+if dogAge > 12 {
+    print("You are elderly <3")
+} else if dogAge < 10 {
+    //Do this instead
+    print("Young puppy!")
+}
+var favoritefood = "Cereal"
+var otherfood = "Chipotle"
+
+if favoritefood != "Chipotle" {
+    print("Bingus")
+}
+
+if otherfood == "Starbucks" {
+    print("Doo Doo")
+} else if "Starbucks" == favoritefood {
+    print("I'm babey")
+} else {
+    print("loser")
+}
+var Jamie = "Jamussy"
+
+if Jamie == "Jamussy" {
+    print("Jamie I long for u")
+}
+
+var bars = "fire"
+if bars == "fire"{
+    print("Barrrrsssss!!!!!!!")
+}
+
